5bf2d231c1fc93fcf2d13adce924088f2138663d
Patrick Keane <keanept@rose-hulman.edu>
2023-11-23 10:51:54 -0500

    changed lualine.

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -86,9 +86,9 @@ local config = {
         lualine_a = { 'mode' },
         lualine_b = { 'branch' },
         lualine_c = { filename, 'diagnostics' },
-        lualine_x = { diff, lsp_server, 'filetype' },
-        lualine_y = { 'progress' },
-        lualine_z = { 'location' }
+        lualine_x = { diff, lsp_server },
+        lualine_y = { 'filetype' },
+        lualine_z = { 'progress' }
     },
     inactive_sections = {
         lualine_a = {},
