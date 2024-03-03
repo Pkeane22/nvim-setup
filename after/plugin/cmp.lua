97997d16b525e93c6ba9ae46fe5fca6fef290994
Pkeane22 <61167933+Pkeane22@users.noreply.github.com>
2024-03-03 17:03:01 -0500

    Fixed errors from disableing tabnine.

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -26,17 +26,17 @@ require('cmp').setup({
             -- in the following line:
             vim_item.kind = lspkind.symbolic(vim_item.kind, {mode = "symbol"})
             vim_item.menu = source_mapping[entry.source.name]
-            if entry.source.name == "cmp_tabnine" then
-                local detail = (entry.completion_item.labelDetails or {}).detail
-                vim_item.kind = ""
-                if detail and detail:find('.*%%.*') then
-                    vim_item.kind = vim_item.kind .. ' ' .. detail
-                end
-
-                if (entry.completion_item.data or {}).multiline then
-                    vim_item.kind = vim_item.kind .. ' ' .. '[ML]'
-                end
-            end
+            -- if entry.source.name == "cmp_tabnine" then
+            --     local detail = (entry.completion_item.labelDetails or {}).detail
+            --     vim_item.kind = ""
+            --     if detail and detail:find('.*%%.*') then
+            --         vim_item.kind = vim_item.kind .. ' ' .. detail
+            --     end
+            --
+            --     if (entry.completion_item.data or {}).multiline then
+            --         vim_item.kind = vim_item.kind .. ' ' .. '[ML]'
+            --     end
+            -- end
             local maxwidth = 80
             vim_item.abbr = string.sub(vim_item.abbr, 1, maxwidth)
             return vim_item
@@ -45,7 +45,7 @@ require('cmp').setup({
     sorting = {
         priority_weight = 2,
         comparators = {
-            require('cmp_tabnine.compare'),
+            -- require('cmp_tabnine.compare'),
             compare.offset,
             compare.exact,
             compare.score,
@@ -58,19 +58,19 @@ require('cmp').setup({
     },
 })
 
-require('cmp_tabnine').setup({
-    max_lines = 1000,
-    max_num_results = 20,
-    sort = true,
-    run_on_every_keystroke = true,
-    snippet_placeholder = '..',
-    ignored_file_types = {
-        -- default is not to ignore
-        -- uncomment to ignore in lua:
-        -- lua = true
-    },
-    show_prediction_strength = false
-})
+-- require('cmp_tabnine').setup({
+--     max_lines = 1000,
+--     max_num_results = 20,
+--     sort = true,
+--     run_on_every_keystroke = true,
+--     snippet_placeholder = '..',
+--     ignored_file_types = {
+--         -- default is not to ignore
+--         -- uncomment to ignore in lua:
+--         -- lua = true
+--     },
+--     show_prediction_strength = false
+-- })
 
 
 require("rust-tools").setup({
